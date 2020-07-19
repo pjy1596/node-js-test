edbcee0b12e2b64143566d926840bca225816661
Junyoung Park <park5@ualberta.ca>
2020-07-20 07:33:30 +0900

    secret info

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const { User } = require("./models/User");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://pjy1596:pjy5200@cluster0.hz0eq.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("connected..."))
   .catch((err) => console.log("error..."));
 
-app.get("/", (req, res) => res.send("Hello World!~~~~~수정 중~~~"));
-
+app.get("/", (req, res) => res.send("Hello World!~~~~~a~~~"));
+app.post("/register", (req, res) => {
+  // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
